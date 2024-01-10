1e9a7aea1db5e802ae1396485f4e31f9987740bb
Nickola Trupcheff <nickola.trupcheff@storpool.com>
2024-01-10 15:08:50 +0000

    Welcome message change
    Change 'Welcome to Grafana' to 'Welcome to StorPool Analytics' on emails and pages

--- public/app/core/components/Branding/Branding.tsx
+++ public/app/core/components/Branding/Branding.tsx
@@ -59,8 +59,8 @@ export class Branding {
   static LoginBackground = LoginBackground;
   static MenuLogo = MenuLogo;
   static LoginBoxBackground = LoginBoxBackground;
-  static AppTitle = 'Grafana';
-  static LoginTitle = 'Welcome to Grafana';
+  static AppTitle = 'StorPool Analytics';
+  static LoginTitle = 'Welcome to StorPool Analytics';
   static HideEdition = false;
   static GetLoginSubTitle = (): null | string => {
     return null;
